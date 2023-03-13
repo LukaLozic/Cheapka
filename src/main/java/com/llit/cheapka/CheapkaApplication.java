b44a4145f13cdaa987e071cf5bb2b497ad1a6622
Luka Lozić <luka.lozic@serengetitech.com>
2023-03-13 13:43:45 +0100

    initial commit

---
+++ src/main/java/com/llit/cheapka/CheapkaApplication.java
@@ -0,0 +1,13 @@
+package com.llit.cheapka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheapkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llit/cheapka/CheapkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llit.cheapka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
