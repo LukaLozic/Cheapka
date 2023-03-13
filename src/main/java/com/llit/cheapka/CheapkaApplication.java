978c585fd2edabee852efda59ad994c414818160
LukaLozić <luka.lozic1995@gmail.com>
2023-03-13 15:32:55 +0100

    initial structure setup

--- src/main/java/com/llit/cheapka/CheapkaApplication.java
+++ src/main/java/com/llit/cheapka/CheapkaApplication.java
@@ -1,16 +1,21 @@
 package com.llit.cheapka;
 
+import controllers.CheapkaController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan(basePackageClasses = CheapkaController.class)
 public class CheapkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheapkaApplication.class, args);
 
-        System.out.println("develop");
-
+        System.out.println("works");
     }
 
 }
