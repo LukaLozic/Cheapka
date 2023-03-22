a9089423e2974c75cfa6aa5566610d7568820d19
LukaLozić <luka.lozic1995@gmail.com>
2023-03-22 13:19:25 +0100

    ldap additional setup, added endpoints for searchin ldap user store

--- src/main/java/com/llit/controllers/HomeController.java
+++ src/main/java/com/llit/controllers/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/homse")
     public String index() {
 
-        return "Home page";
+        return "Home pasge";
     }
 }
