d6b2cbe5dd8ff9729143972ce3393bbe822a9736
LukaLozić <luka.lozic1995@gmail.com>
2023-03-22 13:23:16 +0100

    test message

--- src/main/java/com/llit/controllers/HomeController.java
+++ src/main/java/com/llit/controllers/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/hoome/page")
     public String index() {
 
-        return "Home page";
+        return "Hoome page";
     }
 }
