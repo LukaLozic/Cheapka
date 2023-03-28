536f227740294ba07a43450654281445215c0af2
LukaLozic <46923923+LukaLozic@users.noreply.github.com>
2023-03-28 11:52:51 +0200

    Feature/5 jwt setup (#8)
    * ldap additional setup, added endpoints for searchin ldap user store
    
    * ldap additional setup, added endpoints for searchin ldap user store
    
    * refactor of ldap and jwt manipulation
    
    * Update README.md

--- src/main/java/com/llit/CheapkaApplication.java
+++ src/main/java/com/llit/CheapkaApplication.java
@@ -2,12 +2,12 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+ @SpringBootApplication
 public class CheapkaApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CheapkaApplication.class, args);
     }
 
---
+++ src/main/java/com/llit/util/JwtUtil.java
@@ -0,0 +1,60 @@
+package com.llit.util;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    private static final String SECRET_KEY = "244326462948404D635166546A576E5A7234753778214125442A472D4B614E64";
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey("final").parseClaimsJws(token).getBody();
+    }
+
+    private String getSignInKey() {
+
+ /*           byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+            return Keys.hmacShaKeyFor(keyBytes);*/
+        return null;
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
